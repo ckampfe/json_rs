7d0118be205ac69b879425c89f1a15992cbccb6c
Clark Kampfe <clark.kampfe@gmail.com>
2021-01-26 17:20:32 -0600

    big(ger) ints, pretty printing,

--- lib/json_rs.ex
+++ lib/json_rs.ex
@@ -4,7 +4,18 @@ defmodule JsonRs do
 
   use Rustler, otp_app: :json_rs, crate: :json_rs
 
-  def encode(_term), do: error()
+  def encode(term), do: encode(term, false)
+  def encode(_term, _pretty), do: error()
+
+  def encode!(term), do: encode!(term, false)
+
+  def encode!(term, pretty) do
+    case encode(term, pretty) do
+      {:ok, term} -> term
+      {:error, error} -> raise error
+    end
+  end
+
   def decode(_string), do: error()
 
   defp error() do
--- native/json_rs/src/lib.rs
+++ native/json_rs/src/lib.rs
@@ -1,6 +1,12 @@
+use num_traits::cast::ToPrimitive;
 use rustler::{Atom, Error, ListIterator, MapIterator, Term};
-use serde::ser::Error as SerdeError;
 use serde::ser::{SerializeMap, SerializeSeq};
+use serde::{ser::Error as SerdeError, serde_if_integer128};
+use std::slice::from_raw_parts;
+
+const ETF_VERSION_NUMBER_BYTE: u8 = 131;
+const ETF_SMALL_BIG_TAG_BYTE: u8 = 110;
+const ETF_LARGE_BIG_TAG_BYTE: u8 = 111;
 
 mod atoms {
     #[inline]
@@ -13,9 +19,20 @@ mod atoms {
 rustler::init!("Elixir.JsonRs", [encode, decode]);
 
 #[rustler::nif]
-fn encode(t: Term) -> Result<(Atom, String), Error> {
-    let jt: SerdeTerm = t.into();
-    let s = serde_json::to_string(&jt).unwrap();
+fn encode(t: Term, pretty: bool) -> Result<(Atom, String), Error> {
+    let serde_term: SerdeTerm = t.into();
+    let s = if pretty {
+        serde_json::to_string_pretty(&serde_term).map_err(|e| {
+            let r = e.to_string();
+            rustler::Error::RaiseTerm(Box::new(r))
+        })?
+    } else {
+        serde_json::to_string(&serde_term).map_err(|e| {
+            let r = e.to_string();
+            rustler::Error::RaiseTerm(Box::new(r))
+        })?
+    };
+
     Ok((crate::atoms::ok(), s))
 }
 
@@ -24,16 +41,16 @@ fn decode(s: &str) -> Result<Term, Error> {
     todo!()
 }
 
-struct SerdeTerm<'j>(Term<'j>);
+struct SerdeTerm<'t>(Term<'t>);
 
-impl<'j> From<Term<'j>> for SerdeTerm<'j> {
+impl<'t> From<Term<'t>> for SerdeTerm<'t> {
     #[inline]
-    fn from(term: Term<'j>) -> Self {
+    fn from(term: Term<'t>) -> Self {
         SerdeTerm(term)
     }
 }
 
-impl<'j> serde::Serialize for SerdeTerm<'j> {
+impl<'t> serde::Serialize for SerdeTerm<'t> {
     #[inline]
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -45,12 +62,18 @@ impl<'j> serde::Serialize for SerdeTerm<'j> {
                 serializer.serialize_str(s)
             }
             rustler::TermType::Atom => {
-                let s = self.0.atom_to_string().unwrap();
-                if s == "nil" {
-                    serializer.serialize_none()
-                } else {
-                    serializer.serialize_str(&s)
+                if self.0.as_c_arg() == rustler::types::atom::nil().as_c_arg() {
+                    return serializer.serialize_none();
                 }
+                if self.0.as_c_arg() == rustler::types::atom::true_().as_c_arg() {
+                    return serializer.serialize_bool(true);
+                }
+                if self.0.as_c_arg() == rustler::types::atom::false_().as_c_arg() {
+                    return serializer.serialize_bool(false);
+                }
+
+                let s = self.0.atom_to_string().unwrap();
+                serializer.serialize_str(&s)
             }
             rustler::TermType::Map => {
                 let s: MapIterator = self.0.decode().unwrap();
@@ -66,7 +89,7 @@ impl<'j> serde::Serialize for SerdeTerm<'j> {
             rustler::TermType::List => {
                 let s: ListIterator = self.0.decode().unwrap();
                 let size = self.0.list_length().unwrap();
-                let mut seq = serializer.serialize_seq(Some(size)).unwrap();
+                let mut seq = serializer.serialize_seq(Some(size))?;
                 for el in s {
                     let el: SerdeTerm = el.into();
                     seq.serialize_element(&el)?;
@@ -88,6 +111,7 @@ impl<'j> serde::Serialize for SerdeTerm<'j> {
                 if let Ok(s) = self.0.decode() {
                     return serializer.serialize_u64(s);
                 }
+
                 #[cfg(target_pointer_width = "32")]
                 if let Ok(s) = self.0.decode() {
                     return serializer.serialize_i32(s);
@@ -100,6 +124,56 @@ impl<'j> serde::Serialize for SerdeTerm<'j> {
                 if let Ok(s) = self.0.decode() {
                     return serializer.serialize_u32(s);
                 }
+
+                serde_if_integer128! {
+                    let binary = unsafe {
+                        rustler::wrapper::env::term_to_binary(
+                            self.0.get_env().as_c_arg(),
+                            self.0.as_c_arg(),
+                        )
+                        .unwrap()
+                    };
+                    let binary_bytes = unsafe { from_raw_parts(binary.data, binary.size) };
+
+                    match binary_bytes {
+                        [ETF_VERSION_NUMBER_BYTE, ETF_SMALL_BIG_TAG_BYTE, _length_byte, sign_byte, digits @ ..] =>
+                        {
+                            let sign = if sign_byte == &1u8 {
+                                num_bigint::Sign::Minus
+                            } else {
+                                num_bigint::Sign::Plus
+                            };
+
+                            let sb = num_bigint::BigInt::from_radix_le(sign, &digits, 256).unwrap();
+
+                            if let Some(i) = sb.to_u128() {
+                                return serializer.serialize_u128(i);
+                            }
+                            if let Some(i) = sb.to_i128() {
+                                return serializer.serialize_i128(i);
+                            }
+                        }
+                        [ETF_VERSION_NUMBER_BYTE, ETF_LARGE_BIG_TAG_BYTE, _length_byte_1, _length_byte_2, _length_byte_3, _length_byte_4, sign_byte, digits @ ..] =>
+                        {
+                            let sign = if sign_byte == &1u8 {
+                                num_bigint::Sign::Minus
+                            } else {
+                                num_bigint::Sign::Plus
+                            };
+
+                            let sb = num_bigint::BigInt::from_radix_le(sign, &digits, 256).unwrap();
+
+                            if let Some(i) = sb.to_u128() {
+                                return serializer.serialize_u128(i);
+                            }
+                            if let Some(i) = sb.to_i128() {
+                                return serializer.serialize_i128(i);
+                            }
+                        }
+                        _ => ()
+                    }
+                }
+
                 Err(S::Error::custom(format!(
                     "unable to serialize {:?} as number",
                     self.0
